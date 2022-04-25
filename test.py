97241abfcd2af0b537cd26c6c01df7fa5c03823a
git <2917021186@qq.com>
2022-04-25 15:15:27 +0800

    Refactor: 重构测速用例

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
@@ -42,12 +43,40 @@ def main():
             nms_boxes = nms(
                 bboxes[idx],
                 iou_threshold=0.5,
-                threshold=0.5,
+                threshold=0.2,
                 box_mode='midpoint'
             )
 
             plot_image(x[idx].permute(1, 2, 0).to('cpu'), nms_boxes, 'midpoint')
 
 
+def detect(img_path):
+    img = Image.open(img_path)
+
+    model = YOLO(S=7, B=2, C=1)
+    # load model from disk
+    model.load_state_dict(torch.load('./doc/yolov1-weight-2.pt'))
+    # evaluation mode
+    model.eval()
+
+    transform = transforms.Compose([transforms.Resize((448, 448)), transforms.ToTensor()])
+    x = transform(img)
+
+    x = x.reshape((1, 3, 448, 448))
+    predictions = model(x)
+    bboxes = cellboxes_to_boxes(predictions)
+
+    nms_boxes = nms(
+        bboxes[0],
+        # 0.5
+        iou_threshold=0.8,
+        threshold=0.3,
+        box_mode='midpoint'
+    )
+
+    plot_image(x[0].permute(1, 2, 0).to('cpu'), nms_boxes, 'midpoint')
+
+
 if __name__ == '__main__':
-    main()
+    # test4 test1 iou_threshold=0.8
+    detect('./img/test/test2.png')
